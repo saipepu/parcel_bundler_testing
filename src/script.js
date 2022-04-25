7c0af04e01debcadb3514a9a0290a674f785e2fc
Sai Pe Pu <saipepu@P3bn.local>
2022-04-25 22:33:51 +0630

    first commit

---
+++ src/script.js
@@ -0,0 +1,13 @@
+import { bro } from "./bro";
+import './styles/main.scss';
+import ScrollMagic from 'scrollmagic'
+
+var controller = new ScrollMagic.Controller();
+
+new ScrollMagic.Scene({
+  duration: 500,
+  offset: 50
+}).setPin("body")
+.addTo(controller);
+
+document.querySelector('h1').textContent = bro(`How's it going`);
